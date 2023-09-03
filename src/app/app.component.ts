722ffb6265a34f03641614ae5049638c3a4fb96d
Daniel Kaźmierczak <daniel.kazmierczak@nekken.pl>
2023-09-03 13:23:40 +0200

    angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {User} from "./models/user.model";
+import {AuthenticationService} from "./services/authentication.service";
+import {Router} from "@angular/router";
+import {Role} from "./models/role.enum";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front-book-seller';
+  title = 'Księgarnia internetowa';
+  currentUser: User = new User;
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+    this.authenticationService.currentUser.subscribe(user => {
+      this.currentUser = user;
+    })
+  }
+
+  isAdmin() {
+    return this.currentUser?.role === Role.ADMIN;
+  }
+
+  logOut() {
+    this.authenticationService.logOut();
+    this.router.navigate(['/login']);
+  }
 }
